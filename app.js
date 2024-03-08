af7506005e549b191b0aba2966e517188ac23fee
Magda-J <magdalena.jakonska@gmail.com>
2024-03-08 11:03:04 +0000

    mongoose

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+mongoose
+  .connect(process.env.CONNECTION_STRING)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err.message);
+  });
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
+
+
+module.exports = app;
