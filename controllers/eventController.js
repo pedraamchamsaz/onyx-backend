49df4fe78ada577a908cc0cc1f1f7eb755999f47
Magda-J <magdalena.jakonska@gmail.com>
2024-03-21 17:54:07 +0000

    update

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,4 +1,5 @@
 const { Event } = require("../models/events");
+const { Users } = require("../models/users")
 
 
 
@@ -14,6 +15,10 @@ exports.getAllEvents = async function (req, res) {
     }
 };
 
+
+
+
+
 exports.getSingleEvent = async function (req, res) {
   try {
     const eventId = req.params.eventid
@@ -28,27 +33,29 @@ exports.getSingleEvent = async function (req, res) {
 
 
 
+
 exports.createEvent = async function (req, res) {
    try { 
     const newEvent = req.body;
   
-    // // Retrieve the user's token from the authorization header
-    // const authHeader = req.headers["authorization"];
-  
+    // Retrieve the user's token from the authorization header
+    const authHeader = req.headers["authorization"];
+    console.log("auth stuff" ,authHeader)
+    console.log("req head", req.headers)
     
-    //   // Find the user based on the token
-    //   const user = await User.findOne({ token: authHeader });
+      // Find the user based on the token
+      const user = await Users.findOne({ token: authHeader });
   
-    //   // Check if the user exists
-    //   if (!user) {
-    //     return res.sendStatus(403); // Forbidden if user not found
-    //   }
+      // Check if the user exists
+      if (!user) {
+        return res.sendStatus(403); // Forbidden if user not found
+      }
   
      
-      // Create a new event document and associate it with the user
+    //   // Create a new event document and associate it with the user
       const event = new Event({
         ...newEvent,
-        // createdBy: user._id, // Assign the user's ID to the createdBy field
+        createdBy: user._id, // Assign the user's ID to the createdBy field
       });
   
       // Save the event
@@ -62,3 +69,31 @@ exports.createEvent = async function (req, res) {
   };
 
 
+
+
+
+ 
+    exports.getUserEvents = async function (req, res) { 
+    try {
+      // Retrieve the user's token from the authorization header
+      const authHeader = req.headers["Authorization"];
+  console.log(authHeader)
+      // Find the user based on the token
+      const user = await Users.findOne({ token: authHeader });
+      console.log(user)
+  
+      // Check if the user exists
+      if (!user) {
+        return res.sendStatus(403); // Forbidden if user not found
+      }
+  
+      // Find events created by the user
+      const userEvents = await Event.find({ createdBy: user._id });
+  
+      res.send(userEvents);
+    } catch (error) {
+      console.error("Error fetching user events:", error);
+      res.status(500).send({ message: "Error fetching user events." });
+    }
+  };
+ 
--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,13 +1,15 @@
 const express = require ("express")
 const router = express.Router()
 
-const {getAllEvents, createEvent, getSingleEvent} = require ("../controllers/eventController");
+const {getAllEvents, createEvent, getSingleEvent, getUserEvents} = require ("../controllers/eventController");
 const { addParticipant } = require("../controllers/bookingController");
 const { createUser } = require("../controllers/authController");
+const { authUser } = require("../middleware/middleware");
 
 router.get("/", getAllEvents); // /events
+router.get("/userEvents", getUserEvents);
 router.get("/:eventid", getSingleEvent) // /events/1234567
-router.post("/postevent", createEvent); // /events/postevent
+router.post("/postevent", createEvent, authUser); // /events/postevent
 router.post('/:eventid/:dateindex', addParticipant) // /events/1234567/0
 router.post("/signup", createUser)
 
